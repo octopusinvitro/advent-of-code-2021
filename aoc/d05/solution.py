ccb68678ad48462e0e4625a0c909ca2ae93b8226
octopusinvitro <octopusinvitro@users.noreply.github.com>
2021-12-05 22:25:41 +0000

    Add day 05 solution

---
+++ aoc/d05/solution.py
@@ -0,0 +1,28 @@
+from .line import Line
+
+
+class Solution:
+    def __init__(self, lines):
+        self._lines = lines
+
+    def part1(self):
+        map = {}
+
+        for line in self._lines:
+            line = Line(line)
+            if line.diagonal():
+                continue
+
+            for point in line.points():
+                map[point] = map.get(point, 0) + 1
+
+        return sum(1 if count > 1 else 0 for count in map.values())
+
+    def part2(self):
+        map = {}
+
+        for line in self._lines:
+            for point in Line(line).points():
+                map[point] = map.get(point, 0) + 1
+
+        return sum(1 if count > 1 else 0 for count in map.values())

86a047d7fbce7d3c7f0c1edd37e4f3210e9867c6
octopusinvitro <octopusinvitro@users.noreply.github.com>
2021-12-08 16:28:56 +0000

    Add day 07 solution

--- tests/common.py
+++ tests/common.py
@@ -3,3 +3,7 @@ from os import path, getcwd
 
 def fixture_path(day, filename):
     return path.join(getcwd(), 'tests', day, 'fixtures', filename)
+
+
+def data_path(day):
+    return path.join(getcwd(), 'data', day + '_input')
